26337a74a10f073efbfbd7264bba7e70b2282adb
VANTIENHOVENMT21 <vanTienhovenMT21@gcc.edu>
2023-03-01 10:38:22 -0500

    Hello Colton

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Please give me your name");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println("Hello " + name);
     }
 }
